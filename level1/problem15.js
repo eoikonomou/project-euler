2bc911bc1fa1a4cb0fb01013d90bd13001bf0039
Efstathios Oikonomou <eusoikonomou@gmail.com>
2019-07-19 15:16:58 +0300

    p15 solution

--- level1/problem15.js
+++ level1/problem15.js
@@ -6,26 +6,16 @@ Starting in the top left corner of a 2×2 grid, and only being able to move to t
 
 How many such routes are there through a 20×20 grid?
 */
-
-function findPoints(i, j, max) {
-  if ((i === max) && (j === max)) {
-    return 1;
-  }
-  if (i < max && j < max) {
-    return findPoints(i + 1, j, max) + findPoints(i, j + 1, max);
-  }
-  if (i < max) {
-    return findPoints(i + 1, j, max);
-  }
-  return findPoints(i, j + 1, max);
-}
-
 function latticePaths(n) {
   const now = Date.now();
-  const paths = findPoints(0, 0, n);
+  let paths = 1;
+  for (let i = 0; i < n; i++) {
+    paths *= (2 * n) - i;
+    paths /= i + 1;
+  }
   console.log(require('../utils/time')(now));
   return paths;
 }
 
-// console.log(latticePaths(2)); // 6
-console.log(latticePaths(20)); //
+console.log(latticePaths(2)); // 6
+console.log(latticePaths(20)); // 137846528820
