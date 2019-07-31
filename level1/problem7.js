87d6d8326e62d49116f15c918e336c256222664e
Efstathios Oikonomou <eusoikonomou@gmail.com>
2019-07-31 08:59:14 +0300

    solved problem 27

--- level1/problem7.js
+++ level1/problem7.js
@@ -10,6 +10,9 @@ function isPrime(number) {
   if (number < 4) {
     return true;
   }
+  if (number % 2 === 0) {
+    return false;
+  }
   for (let i = 3; i <= Math.sqrt(number); i += 2) {
     if (number % i === 0) {
       return false;
--- level2/problem27.js
+++ level2/problem27.js
@@ -1,3 +1,5 @@
+const bigInt = require('big-integer');
+const isPrime = require('../level1/problem7').isPrime;
 // Quadratic primes
 
 /*
@@ -17,3 +19,42 @@ where |n| is the modulus/absolute value of n
 e.g. |11|=11 and |−4|=4
 Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
 */
+
+function quadraticFunction(a, b, n) {
+  return bigInt(n)
+    .pow(bigInt(2))
+    .add(bigInt(a).multiply(bigInt(n)))
+    .add(bigInt(b));
+}
+
+function findProductOfCoefficientsWithMaxNumberOfPrimes(aLimit, bLimit) {
+  const now = Date.now();
+  var maxConsecutivePrimesCount = 0;
+  var coefficients = {
+    a: 0,
+    b: 0
+  };
+  for (let a = -aLimit + 1; a < aLimit; a += 2) {
+    for (let b = 0; b <= bLimit; b += 1) {
+      for (i = 0; i < 2; i++) {
+        const sign = i === 0 ? 1 : -1;
+        const aBalancer = b % 2 === 0 ? -1 : 0; // In order for the first element of the function to be odd and prime b + a must be even
+        let n = 0;
+        while (isPrime(Math.abs(quadraticFunction(a + aBalancer, sign * b, n).toJSNumber()))) {
+          n++;
+        }
+        if (n > maxConsecutivePrimesCount) {
+          coefficients = {
+            a: a + aBalancer,
+            b: sign * b
+          };
+          maxConsecutivePrimesCount = n;
+        }
+      }
+    }
+  }
+  console.log(require('../utils/time')(now));
+  return coefficients.a * coefficients.b;
+}
+
+console.log(findProductOfCoefficientsWithMaxNumberOfPrimes(1000, 1000)); // -59231
