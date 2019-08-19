5acc3c1ee21b3076bb56a3db1db0bcbb0f1ef065
Efstathios Oikonomou <eusoikonomou@gmail.com>
2019-08-19 15:04:46 +0300

    completed 47 - completed level 2

--- level2/problem47.js
+++ level2/problem47.js
@@ -10,23 +10,33 @@ The first three consecutive numbers to have three distinct prime factors are:
 
 644 = 2² × 7 × 23
 645 = 3 × 5 × 43
-646 = 2 × 17 × 19.
+646 = 2 × 17 × 19
 
 Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
 */
 
-function findFirstOf4() {
+function consecutiveIntegers(maxNumber, consecutive) {
   const now = Date.now();
-  const primes = [2];
-  console.log('populating primes...');
-  for (let i = 3; i < 10000; i += 2) {
-    if (require('../level1/problem7').isPrime(i)) {
-      primes.push(i);
+  let factorCounts = [];
+  for (let i = 0; i < maxNumber; i++) {
+    factorCounts.push(0);
+  }
+  let contiguousFours = 0;
+  for (let i = 2; i < factorCounts.length; ++i) {
+    contiguousFours = factorCounts[i] === consecutive ? contiguousFours + 1 : 0;
+    if (contiguousFours === consecutive) {
+      console.log(require('../utils/time')(now));
+      return i - 3;
+    }
+    if (factorCounts[i] === 0) // It's Prime
+    {
+      for (let j = i * 2; j < factorCounts.length; j += i) {
+        ++factorCounts[j];
+      }
     }
   }
-  let number = 1;
-  let consecutiveIntegers = [];
-  while (consecutiveIntegers.length === 0) {
-
-  }
+  console.log(require('../utils/time')(now));
+  return -1;
 }
+
+console.log(consecutiveIntegers(1000000, 4));
